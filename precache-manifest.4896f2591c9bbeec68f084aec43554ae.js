3708cbb91cd32e3a23797efa3d74e86f94239953
Asfaw Nahom <s2200108@edu.bc.fi>
2022-10-07 13:19:26 +0300

    Updates

---
+++ precache-manifest.4896f2591c9bbeec68f084aec43554ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59b528b3ffd0c113cd37",
+    "url": "/nahomzoe/countries-API-using-Redux.git/static/css/main.98bf4611.chunk.css"
+  },
+  {
+    "revision": "59b528b3ffd0c113cd37",
+    "url": "/nahomzoe/countries-API-using-Redux.git/static/js/main.59b528b3.chunk.js"
+  },
+  {
+    "revision": "d768d68c3752f72cfc4e",
+    "url": "/nahomzoe/countries-API-using-Redux.git/static/css/1.f52e8434.chunk.css"
+  },
+  {
+    "revision": "d768d68c3752f72cfc4e",
+    "url": "/nahomzoe/countries-API-using-Redux.git/static/js/1.d768d68c.chunk.js"
+  },
+  {
+    "revision": "a67ca9646ab6fa242a59",
+    "url": "/nahomzoe/countries-API-using-Redux.git/static/js/runtime~main.a67ca964.js"
+  },
+  {
+    "revision": "4df0ddb2740708f23be82c845f3be5a1",
+    "url": "/nahomzoe/countries-API-using-Redux.git/index.html"
+  }
+];
